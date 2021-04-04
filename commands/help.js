0e1928998e5149a5678366399f76d630853a17b2
ten_devoleper <60765259+tenDevoleper@users.noreply.github.com>
2021-04-04 21:36:04 +0700

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -5,6 +5,8 @@ module.exports = {
   aliases: ["h", "cmd", "command"],
   run: async (client, message, args) => {
     let embed = new Discord.MessageEmbed()
+      .setColor("RANDOM")
+      .setThumbnail("https://cdn.discordapp.com/attachments/700871396540284978/828276493418758154/giphy_1.gif")
       .setTitle(`Commands`)
       .setDescription(client.commands.map(cmd => `\`${cmd.name}\``).join(", "))
       .setTimestamp()
