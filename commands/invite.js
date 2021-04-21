40bd640fdac96bf3e7adb9db5e2e8d97950c9378
ahjmrwesite <82580347+ahjmrwesite@users.noreply.github.com>
2021-04-22 05:12:19 +0700

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -13,7 +13,7 @@ I am a bot from Cambodia.
 I made it with JavaScript programming language with discord.js as a framework,
 don't forget to follow my social media
 
-Invite the bot [ [Click Here](https://discord.com/oauth2/authorize?client_id=${client.id}&scope=bot&permissions=2148846673) ], Join support server [ [click here](https://discord.gg/VYRyTJ3) ]
+Invite the bot [ [Click Here](https://discord.com/oauth2/authorize?client_id=${client.id}&scope=bot&permissions=2148846673) ], Join support server [ [click here](https://discord.gg/BmC5uR6X) ]
     `)
     message.channel.send(embed);
     message.react("📄")
